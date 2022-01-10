2995efab500a3752700feb931107624571438678
RahulBorade94 <boraderahul94@gmail.com>
2022-01-10 23:21:09 +0530

    Calcualte Employee Wage

--- src/com/bridgelabz/employeewage/EmployeeWageProgram.java
+++ src/com/bridgelabz/employeewage/EmployeeWageProgram.java
@@ -2,35 +2,40 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWageProgram {
 
-    public static final int IS_PART_TIME = 0;
-    public static final int IS_FULL_TIME = 1;
-    public static final int EMP_WAGE_PER_HOUR = 20;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
 
-    public static int addPartTimeEmpWage() {
+    public static int calculateEmpWage() {
 
         int empWage = 0;
         int empHrs = 0;
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME)
-            empHrs = 4;
-        else if (empCheck == IS_PART_TIME)
-            empHrs = 8;
-        else
-            empHrs = 0;
+        switch (empCheck) {
 
-        empWage = empHrs * EMP_WAGE_PER_HOUR;
-        System.out.println("Emp wage " + empWage);
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("EmployeeWage:"+ empWage);
 
         return empWage;
     }
 
 
-        public static void main(String[] args) {
-            addPartTimeEmpWage();
+        public static void main (String[]args){
+            calculateEmpWage();
 
         }
-}
+    }
 
 
 
