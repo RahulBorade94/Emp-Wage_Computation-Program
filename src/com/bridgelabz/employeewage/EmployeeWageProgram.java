4678301ff3a12d4c296c1821b591577c8113a567
RahulBorade94 <boraderahul94@gmail.com>
2022-01-13 19:00:43 +0530

    Calcualting Wages for a Month

--- src/com/bridgelabz/employeewage/EmployeeWageProgram.java
+++ src/com/bridgelabz/employeewage/EmployeeWageProgram.java
@@ -5,37 +5,46 @@ public class EmployeeWageProgram {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_WORKING_DAY = 2;
 
-    public static int calculateEmpWage() {
+    public static int calculateEmpWageMonth() {
 
         int empWage = 0;
         int empHrs = 0;
+        int totalEmpWage = 0;
+        for (int day = 1; day < NUM_WORKING_DAY; day++) {
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
 
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
 
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("EmployeeWage:" + empWage);
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("EmployeeWage:"+ empWage);
+        totalEmpWage += empWage;
+        System.out.println("Total EmpWage:"+ totalEmpWage);
+        return totalEmpWage;
 
-        return empWage;
+}
+
+    public static void main(String[] args) {
+        calculateEmpWageMonth();
+
     }
+}
 
 
-        public static void main (String[]args){
-            calculateEmpWage();
-
-        }
-    }
 
 
 
